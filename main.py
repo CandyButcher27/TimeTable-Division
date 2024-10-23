fa25ae4460afa42a6a31fe4d370dbb3b54a5b738
CandyButcher27 <srivastavaaryaman555@gmail.com>
2024-10-23 17:48:24 +0530

    Cleaning JSon
    Made sure that the json file output looks elegant

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ json_file_path = "extracted_Data.json"
 
 table_data = []
 
+#Extracting the data
 with pdfplumber.open(file_path) as pdf :
 
     for page_num in range(len(pdf.pages)):
@@ -19,10 +20,42 @@ with pdfplumber.open(file_path) as pdf :
                row_data = {headers[i]:row[i]  for i in range(len(headers))}
                table_data.append(row_data)
 
+#Putting the data in a json file 
 with open(json_file_path,'w') as json_file :
     json.dump(table_data,json_file,indent=4)
 
-print(f"The data has been saved into the {json_file_path} in a proper format")
+#Cleaning the data
+with open(json_file_path,'r') as file:
+    data = json.load(file)
+
+    previous_values={}
+
+    for entry in data :
+        
+        for key,value in entry.items():
+            if value=="":
+                entry[key] = previous_values.get(key,"")
+            else :
+                previous_values[key] = value
+        
+        if 'SlNo' in entry :
+            entry['SlNo'] = entry['SlNo'].replace(')','')
+        
+        if 'Course Title' in entry :
+            entry['Course Title'] = entry['Course Title'].replace('\n','')
+        
+        if 'New' in entry :
+            entry['New'] = entry['New'].replace('\n','')
+        
+        if 'Existing' in entry:
+            entry['Existing'] = entry['Existing'].replace('\n','')
+
+
+with open(json_file_path,'w') as file:
+    json.dump(data,file,indent=4)
+
+print("Json has been formatted")
+    
     
 
         
