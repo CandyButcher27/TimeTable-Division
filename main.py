20137aa9d1af19cd4fd024a78edbc42360237681
CandyButcher27 <srivastavaaryaman555@gmail.com>
2024-10-23 17:19:17 +0530

    Json Format
    Converted the extracted data into json format as well as corrected a few bugs in the code

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pdfplumber
+import json
 
 file_path = "C:/Users/sriva/Desktop/TTD/TimeTable-Division/TT CHANGES - 5.pdf"
+json_file_path = "extracted_Data.json"
 
 table_data = []
 
@@ -14,11 +16,13 @@ with pdfplumber.open(file_path) as pdf :
             if(page_num==0):
                 headers = table[0]
             for row in table[1:]:
-               for i in range(len(headers)):
-                   row_data = {headers[i]:row[i]}
-                   table_data.append(row_data)
+               row_data = {headers[i]:row[i]  for i in range(len(headers))}
+               table_data.append(row_data)
 
-    print(table_data)
+with open(json_file_path,'w') as json_file :
+    json.dump(table_data,json_file,indent=4)
+
+print(f"The data has been saved into the {json_file_path} in a proper format")
     
 
         
